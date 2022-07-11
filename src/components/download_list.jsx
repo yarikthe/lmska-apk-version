080ab1b43e61976cf0aee7e73b8bc8bcfc4113d3
Yaroslav Lukyanchuk <yarikthe@gmail.com>
2022-07-11 19:14:08 +0300

    v.1.8.2 build:2

--- src/components/download_list.jsx
+++ src/components/download_list.jsx
@@ -10,6 +10,7 @@ function DownloadList() {
                 <h2>Version</h2>
                 <hr/>
             </div>
+            <Download url={'/lmska-apk-version/download/app-release-v1.8.2_build-2.apk'} title={"LMSka Study v.1.8.2 build:2"}/>
             <Download url={'/lmska-apk-version/download/app-release-v1.8.1_build-10.apk'} title={"LMSka Study v.1.8.1 build:10"}/>
             <Download url={'/lmska-apk-version/download/app-release-v1.8.1_build-7.apk'} title={"LMSka Study v.1.8.1 build:7"}/>
             <Download url={'/lmska-apk-version/download/app-release-v1.8.1_build-3.apk'} title={"LMSka Study v.1.8.1 build:3"}/>
